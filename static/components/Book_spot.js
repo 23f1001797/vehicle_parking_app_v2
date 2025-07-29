4a2a6afd3129b662ea106db28d5f0e2946e3a0a6
Hemlata Mahawar <23f1001797@ds.study.iitm.ac.in>
2025-07-29 17:09:04 +0530

    Milestone-VP-MAD2 User-Dashboard-Management

---
+++ static/components/Book_spot.js
@@ -0,0 +1,97 @@
+export default {
+    name: 'BookSlot',
+    template: `
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 bg-light p-4 rounded shadow-sm">
+                        <h4 class="text-center fw-bold mb-4">Book The Parking Spot</h4>
+                        <form @submit.prevent="bookspot">
+                            <div class="mb-3">
+                                <label for="spotID" class="form-label">Spot ID:</label>
+                                <input type="text" class="form-control" id="spotID" v-model="formData.spot_id" required readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="lotID" class="form-label">Lot ID:</label>
+                                <input type="number" class="form-control" id="lotID" v-model.number="formData.lot_id" required readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="pl_name" class="form-label">Prime Location Name:</label>
+                                <input type="text" class="form-control" id="pl_name" v-model="formData.pl_name" required readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="address" class="form-label">Address:</label>
+                                <input type="text" class="form-control" id="address" v-model="formData.address" required readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="userID" class="form-label">User ID:</label>
+                                <input type="text" class="form-control" id="userID" v-model="formData.user_id" required readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="vrno" class="form-label">Vehical No.:</label>
+                                <input type="text" class="form-control" id="vrno" v-model="formData.vrn" required>
+                            </div>
+                            <div class="mt-4 d-flex justify-content-between">
+                                <button type="submit" class="btn btn-primary">Reserve</button>
+                                <button class="btn btn-outline-secondary">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row text-center" v-if="message">
+                {{ message }}
+                </div>
+            </div>`,
+    data() {
+        return {
+            formData: {
+                spot_id: "",
+                lot_id: "",
+                pl_name: "",
+                address: "",
+                user_id: "",
+                vrn: ''
+            },
+            message: ''
+
+        }
+    },
+    mounted() {
+        this.loadData()
+    },
+    methods: {
+        loadData() {
+            const lot_id = this.$route.params.lot_id
+            fetch(`/api/${lot_id}/book`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authentication-Token": localStorage.getItem("auth_token")
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.formData = data
+                })
+        },
+        bookspot() {
+            fetch(`/api/reserve_slot/${this.formData.user_id}/create/${this.formData.spot_id}`, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authentication-Token": localStorage.getItem("auth_token")
+                },
+                body: JSON.stringify({ "vrn": this.formData.vrn })
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.error) {
+                        this.message = data.error
+                    } else {
+                        this.message = data.message
+                        this.$router.push(`/user/${this.formData.user_id}/dashboard`)
+                    }
+                })
+        }
+    }
+
+}
---
+++ static/components/Release_spot.js
@@ -0,0 +1,119 @@
+export default {
+    name: 'ReleaseSpot',
+    template: `
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 bg-light p-4 rounded shadow-sm">
+                        <h4 class="text-center fw-semibold mb-4">Release The Parking Spot</h4>
+                        <form @submit.prevent="release">
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="spotID" class="form-label">Spot ID:</label>
+                                <input type="text" class="form-control" id="spotID" v-model="formData.spot_id" required readonly>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="lotID" class="form-label">Lot ID:</label>
+                                <input type="text" class="form-control" id="lotID" v-model="formData.lot_id" required readonly>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="price" class="form-label">Lot Price/hr:</label>
+                                <input type="text" class="form-control" id="price" v-model="formData.price" required readonly>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="vrno" class="form-label">Vehical No.:</label>
+                                <input type="text" class="form-control" id="vrno" v-model="formData.vrn" required readonly>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="parkingtime" class="form-label">Parking Timestamp:</label>
+                                <input type="text" class="form-control" id="parkingtime" v-model="formData.parking_timestamp" required readonly>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="releasingtime" class="form-label">Releasing Timestamp:</label>
+                                <input type="text" class="form-control" id="releasingtime" v-model="formData.leaving_timestamp" required readonly>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="duration" class="form-label">Duration:</label>
+                                <input type="text" class="form-control" id="duration" v-model="formData.duration" required readonly>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="totalcost" class="form-label">Total Cost:</label>
+                                <input type="text" class="form-control" id="totalcost" v-model="formData.parking_cost" required readonly>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <button type="submit" class="btn btn-primary">Release</button>
+                            <button  class="btn btn-outline-secondary">Cancel</button>
+                        </div>
+                        </form>
+                        <div class="row text-center" v-if="message">
+                        {{ message }}
+                        </div>
+                    </div>
+                </div>
+            </div>`,
+    data() {
+        return {
+            formData: {
+                reservation_id: "",
+                lot_id: "",
+                price: "",
+                spot_id: "",
+                user_id: "",
+                vrn: '',
+                parking_timestamp: '',
+                leaving_timestamp: '',
+                duration: '',
+                status: '',
+                pl_name: '',
+                parking_cost: ''
+            },
+            message: ''
+
+        }
+    },
+    mounted() {
+        this.loadData()
+    },
+    methods: {
+        loadData() {
+            const reserve_id = this.$route.params.reservation_id
+            fetch(`/api/reservation/${reserve_id}/get`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authentication-Token": localStorage.getItem("auth_token")
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.formData = data
+                })
+        },
+        release() {
+            const reserve_id = this.$route.params.reservation_id
+            fetch(`/api/reservation/${reserve_id}/release`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authentication-Token": localStorage.getItem("auth_token")
+                }
+            }).then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.error) {
+                        this.message = data.error
+                    } else {
+                        this.message = data.message
+                        this.$router.push(`/user/${this.formData.user_id}/dashboard`)
+                    }
+                })
+        }
+    }
+
+}
